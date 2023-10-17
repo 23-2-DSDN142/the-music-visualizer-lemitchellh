03f4228b83deef4b7bd0f39ee3b310371d745e1b
lemitchellh <132876848+lemitchellh@users.noreply.github.com>
2023-10-17 20:44:34 +1300

    17/10

--- music_starter.js
+++ music_starter.js
@@ -1,16 +1,17 @@
 let rotationOffset = 0;
 
-  
+
+
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Comic Sans'); // please use CSS safe fonts
 createCanvas(windowWidth,windowHeight) 
-background ( 10, 10, bass/4)  
+background (10, 10, bass/4)  
 rectMode(CENTER)
   angleMode(DEGREES)
   textSize(44);
-
+  
   var basscolor = map(bass, 0, 255, 190, 240)
   var drumcolor = map(drum, 0, 255, 150, 240)
 
@@ -19,82 +20,90 @@ rectMode(CENTER)
   var bassbar = map(bass, 0, 100, 60, 95)
   var rotatespeed = map(bass, 0, 100, 7, 35)
 
-  let middlewidth = width / 2
-  let middleheight = height / 2
+var vocalcu = map(vocal,0, 100, 1, 5)
 
-  let drumcircle = drumshe * 1.5
+  let middlewidth = width / 2;
+  let middleheight = height / 2;
 
+  let drumcircle = drumshe * 1.5;
+
+ let xcurve = 0
+ let ycurve = height/2
+  
+     
+if(bass<50){
+  bass =0 
+}
 
   
-  // if (bass > 25) {
-  //   strokeWeight(15)
-  //   //fill(50, 110, 150, 155)
-  //   ellipse(middlewidth, middleheight, basssize * 7, basssize * 7)
-
-  // }
-
-
-  // if (bass > 50) {
-  //   strokeWeight(50 + basssize)
-  //   fill(50, 150, 150, 155)
-  //   ellipse(middlewidth * bass, middleheight * bass, basssize * 7, basssize * 7)
-  // }
-
-
-
   
-  stroke(100 + vocal, drumcolor, 50 + drumcolor)
-  strokeWeight(basssize / 100);
+  
+  push()
+  
+strokeWeight(7)
+stroke(20,205, 220)
+fill(10, 10, bass/4)
+  beginShape()
+  curveVertex(xcurve-180,ycurve)
+  curveVertex(xcurve-90,ycurve/vocalcu)
+curveVertex(xcurve,ycurve)
+curveVertex(xcurve+90,ycurve/vocalcu)
+curveVertex(xcurve+180,ycurve)
+curveVertex(xcurve+270,ycurve/vocalcu)
+curveVertex(xcurve+360,ycurve)
+curveVertex(xcurve+450,ycurve/vocalcu)
+curveVertex(xcurve+540,ycurve)
+curveVertex(xcurve+630,ycurve/vocalcu)
+curveVertex(xcurve+720,ycurve)
+curveVertex(xcurve+810,ycurve/vocalcu)
+curveVertex(xcurve+900,ycurve)
+curveVertex(xcurve+990,ycurve/vocalcu)
+curveVertex(xcurve+1080,ycurve)
+curveVertex(xcurve+1170,ycurve/vocalcu)
+curveVertex(xcurve+1260,ycurve)
+curveVertex(xcurve+1350,ycurve/vocalcu)
+curveVertex(xcurve+1440,ycurve)
+curveVertex(xcurve+1530,ycurve/vocalcu)
+curveVertex(xcurve+1620,ycurve)
+endShape()
 
+pop()
 
 
   push()
   translate(width / 2, height / 2);
 
-  //translate(p5.Vector(millis() / 2000, bass*2));
-  //scale(bass/100)     
 
   stroke(20, drumcolor, 100 + drumcolor)
   fill(0, 0, 0, 0)
   rotate(rotationOffset)
   for (let i = 0; i < 100; i = i + 3) {
     if (i > 100) {
-      //fill(20, 20, 20, 200);
-      //ellipse(drumshe*2,drumshe,1*drumshe+i, 1*drumshe+i)
-      //strokeWeight(i-basssize)
+      fill(20, 20, 20, 200);
+      ellipse(drumshe*2,drumshe,1*drumshe+i, 1*drumshe+i)
+      strokeWeight(i-basssize)
     } else {
       fill(50, drumshe * 1.2, drumshe * 20, drumshe * .5);
-      //strokeWeight(drumshe/30)
+      
     }
 
-    //e llipse(0,0, 2*bass+i, 2*bass+i)
     rotate(-180)
-    // ellipse(i,i, 5*drumshe+i, 5*drumshe+i)
     ellipse(i / 4, i, 3 * drumshe + i, 3 * drumshe + i)
     ellipse(i, i / 4, 3 * drumshe + i, 3 * drumshe + i)
     rotate(-180)
-    //ellipse(i/2,i*2, 5*basssize+i, 5*basssize+i)
-
+ 
     rotate(-135)
     ellipse(drumcircle, drumcircle, 1.75 * drumshe + i, 1.75 * drumshe + i)
     ellipse(drumcircle + 100, drumcircle + 100, drumshe - i + 10, drumshe - i + 10)
-    //ellipse(i*4,i/4, 3*basssize+i, 7*basssize+i)
-
   }
 
-  //scale (drum)
   
   pop()
   rotationOffset += rotatespeed / 20;
 
 
-  //stroke(vocal, drumcolor, 50 + drumcolor, bass * 3)
-  strokeWeight(6);
-  rectMode(CORNER)
-  angleMode(DEGREES)
+  
 
-
-  //stroke(20,basscolor,10+basscolor)
   push()
   translate(middlewidth, middleheight)
  
@@ -107,14 +116,10 @@ rectMode(CENTER)
     } else {
       
       noStroke()
-      //stroke(20,basscolor,10+basscolor)
-      //strokeWeight(2)
-      //stroke(20,basscolor,10+basscolor)
+      stroke(20,basscolor,10+basscolor)
       push();
       rotate(12.5 * i * (bass / 50))   
       fill(10, bassbar + 50, bassbar + 120, 50 - bassbar/ 20.55)
-      //rect (-175, , 80, 30, 10, 10, 10, 10 )
-      //rect (100, , 80, 30, 10, 10, 10, 10 )
       rect(-275, 0, 80, 30, 10, 10, 10, 10)
       rect(200, 0, 80, 30, 10, 10, 10, 10)
       rotate(45)
@@ -136,7 +141,7 @@ rectMode(CENTER)
       rect(200, 0, 80, 30, 10, 10, 10, 10)
       pop()
       
-      rotate(90)
+      //rotate(90)
 
       
     }
@@ -145,9 +150,9 @@ rectMode(CENTER)
     noStroke()
   }
    pop()
+rotate(90)
 
 
-    }
-
+}  
 
 
